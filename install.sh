8bd75cc9b26567d4335c88dfa1a55267bf0283a9
Shaun O'Neill <shaunoneill7@me.com>
2018-03-11 22:52:06 +0000

    Install

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+touch $HOME/.zsh && touch $HOME/.zcompdump
+rm -rf $HOME/.zsh* && rm -rf $HOME/.zcompdump*
+git clone https://github.com/XeliteXirish/Zsh-Configs.git $HOME/.zsh
+echo "export ZDOTDIR=\$HOME/.zsh" > $HOME/.zshenv
+echo "source \$ZDOTDIR/.zshenv" >> $HOME/.zshenv
+chsh -s $(which zsh)
