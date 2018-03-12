c38b95070169fe8cdae0ad9ac4a59f7938b4330c
Shaun <shaunoneill7@me.com>
2018-03-12 00:49:06 +0000

    Make sure to update zsh

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
+sudo apt install zsh
 touch $HOME/.zsh && touch $HOME/.zcompdump
 rm -rf $HOME/.zsh* && rm -rf $HOME/.zcompdump*
 git clone https://github.com/XeliteXirish/Zsh-Configs.git $HOME/.zsh
 echo "export ZDOTDIR=\$HOME/.zsh" > $HOME/.zshenv
 echo "source \$ZDOTDIR/.zshenv" >> $HOME/.zshenv
 chsh -s $(which zsh)
+zsh
