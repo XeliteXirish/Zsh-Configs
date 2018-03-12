380db1ff124fece44f5ac6d66f45f1adcabf71eb
XeliteXirish <shaunoneill7@gmail.com>
2018-03-12 18:38:42 +0000

    Archey default

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sudo apt install zsh
+sudo apt install zsh archey
 touch $HOME/.zsh && touch $HOME/.zcompdump
 rm -rf $HOME/.zsh* && rm -rf $HOME/.zcompdump*
 git clone https://github.com/XeliteXirish/Zsh-Configs.git $HOME/.zsh
